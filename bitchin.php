145f2271722feac8d9181c5a4a6606d3fa993dbf
monkewiskunde <123627621+monkewiskunde@users.noreply.github.com>
2023-01-26 11:47:23 +0100

    verandering

--- bitchin.php
+++ bitchin.php
@@ -1,1 +1,28 @@
+<?php
+session_start();
+if
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "hi<br>";
+    ?>
+    <form method="post">
+        <input type="text" name="seriousShit" placeholder="Write something" required><br>
+        <button name="button" type="submit" value="1" formtarget="_self">Send it</button><br>
+    </form>
 
+    <?php
+    if ($bitch == true) {
+        echo "well well well";
+    }
+    ?>
+</body>
+</html> 
