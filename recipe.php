21374de294e10e6cf28b6f2bc6f751c1741e0f57
monkewiskunde <depeuterwout@gmail.com>
2023-02-14 15:15:54 +0100

    weg met die lelijke gele balk

--- recipe.php
+++ recipe.php
@@ -228,68 +228,7 @@ if (empty($_POST["logout"]) != true) {
         </div>
     </section>
     <!-- footer -->
-    <footer>
-        <div class="footer">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class=" col-md-12">
-                        <h2>Request  A<strong class="white"> Call  Back</strong></h2>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <form class="main_form">
-                            <div class="row">
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <input class="form-control" placeholder="Name" type="text" name="Name">
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <input class="form-control" placeholder="Email" type="text" name="Email">
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <input class="form-control" placeholder="Phone" type="text" name="Phone">
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <textarea class="textarea" placeholder="Message" type="text" name="Message"></textarea>
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <button class="send">Send</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <div class="img-box">
-                            <figure><img src="images/img.jpg" alt="img" /></figure>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="footer_logo">
-                          <a href="index.php"><img src="images/logo1.jpg" alt="logo" /></a>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <ul class="lik">
-                            <li><a href="index.php">huis</a></li>
-                            <li><a href="about.php">over ons</a></li>
-                            <li class="active"> <a href="recipe.php">Recept</a></li>
-                            <li><a href="blog.php">blog</a></li>
-                            <li><a href="contact.php">Contacteer ons</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="new">
-                            <h3>Newsletter</h3>
-                            <form class="newtetter">
-                                <input class="tetter" placeholder="Your email" type="text" name="Your email">
-                                <button class="submit">Subscribe</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    
     <!-- end footer -->
     <div class="overlay"></div>
     <!-- Javascript files-->
